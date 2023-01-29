8aa8100773b885232d48d1814da8ccc9c82629ef
brimstonetrader <106700187+brimstonetrader@users.noreply.github.com>
2023-01-29 15:31:39 -0600

    made diamonds fall randomly

---
+++ PianoTest/Assets/Scripts/DiamondSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondSpawner : MonoBehaviour
+{
+
+    public GameObject Diamond;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("DropDiamond");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DropDiamond() {
+        for (;;) {
+            Instantiate(Diamond);
+            yield return new WaitForSeconds(Random.Range(1.5f, 3f));
+        }
+    }
+}
